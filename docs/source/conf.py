797e65690f62da3a7e19dade30c1647b259d93ea
CyrilJl <joly.cyril7@gmail.com>
2024-10-16 17:54:50 +0200

    up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Cyril Joly'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon', 'sphinx_copybutton', "sphinx_favicon"]
+extensions = ['sphinx.ext.napoleon', 'sphinx_copybutton']
 
 templates_path = ['_templates']
 exclude_patterns = []
