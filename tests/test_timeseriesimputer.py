1d70b708ca7cacd65f5ae946917f29f7e9d112d2
Cyril <joly.cyril7@gmail.com>
2024-10-17 15:14:02 +0200

    correct tests

--- tests/test_timeseriesimputer.py
+++ tests/test_timeseriesimputer.py
@@ -21,7 +21,7 @@ def test_impute_full_series(generate_data):
     df_imputed = tsi(df)
     
     # Check that there are no more missing values after imputation
-    assert df_imputed.isnull().sum().sum() > df.isnull().sum().sum()
+    assert df_imputed.isnull().sum().sum() < df.isnull().sum().sum()
 
 def test_impute_2(generate_data):
     """Test imputation only after a specific date."""
@@ -35,7 +35,7 @@ def test_impute_2(generate_data):
     df_imputed = tsi(df, after=cutoff_date)
     
     # Check that there are no missing values after the cutoff date
-    assert df_imputed.isnull().sum().sum() > df.isnull().sum().sum()
+    assert df_imputed.isnull().sum().sum() < df.isnull().sum().sum()
 
     # Check that missing values before the cutoff date remain (if there were any)
     before_cutoff_null_count = df.loc[:cutoff_date].isnull().sum().sum()
