50edbe248e000d0cbe98c780982b01be0dbe8121
Cyril <joly.cyril7@gmail.com>
2024-10-17 12:08:21 +0200

    Update __init__.py with generate_random_time_series

--- timefiller/__init__.py
+++ timefiller/__init__.py
@@ -1,4 +1,5 @@
 from ._positive_output_transformer import PositiveOutput
 from ._time_series_imputer import ImputeMultiVariate, TimeSeriesImputer
+from ._random_time_series import generate_random_time_series
 
-__all__ = ['ImputeMultiVariate', 'PositiveOutput', 'TimeSeriesImputer']
+__all__ = ['ImputeMultiVariate', 'PositiveOutput', 'TimeSeriesImputer', 'generate_random_time_series']
