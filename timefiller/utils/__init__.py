8c05d856afb335e36a9f50714ada0c435e3d2551
Cyril <joly.cyril7@gmail.com>
2024-10-18 17:07:22 +0200

    coquille

--- timefiller/utils/__init__.py
+++ timefiller/utils/__init__.py
@@ -1,4 +1,4 @@
 from ._random_time_series import add_mar_nan, generate_random_time_series
-from ._datasets.py import fetch_pems_bay
+from ._datasets import fetch_pems_bay
 
 __all__ = ['add_mar_nan', 'fetch_pems_bay', 'generate_random_time_series']
