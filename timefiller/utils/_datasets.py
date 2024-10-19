be21cd2f5d3bc8ca0e7d4a8a323d1ef151c441a2
CyrilJl <joly.cyril7@gmail.com>
2024-10-19 22:25:06 +0200

    0.1.6

--- timefiller/utils/_datasets.py
+++ timefiller/utils/_datasets.py
@@ -2,17 +2,16 @@ from os import makedirs
 from os.path import exists, expanduser, join
 
 import pandas as pd
-import requests
-import tempfile
 
 
 def fetch_pems_bay(data_home=None) -> pd.DataFrame:
     """
-    Fetch the PeMS-Bay dataset, a real-world dataset of traffic readings from the Bay Area, provided by the California Department of Transportation (Caltrans). 
-    The dataset is available in HDF5 format and can be retrieved from Zenodo.
+    Fetch the PeMS-Bay dataset, a real-world dataset of traffic readings from the Bay Area, provided by the
+    California Department of Transportation (Caltrans). The dataset is available in csv format and can be
+    retrieved from Zenodo.
 
     If the dataset is not already present in the specified directory, it will be downloaded from:
-    https://zenodo.org/records/4263971/files/pems-bay.h5
+    https://zenodo.org/records/5724362/files/PEMS-BAY.csv
 
     Args:
         data_home (str, optional): Directory where the dataset should be saved. 
@@ -22,9 +21,9 @@ def fetch_pems_bay(data_home=None) -> pd.DataFrame:
         pd.DataFrame: A DataFrame containing traffic readings from the PeMS-Bay dataset.
 
     References:
-        PeMS-Bay dataset: https://doi.org/10.5281/zenodo.4263971
+        PeMS-Bay dataset: https://doi.org/10.5281/zenodo.5724362
     """
-    
+
     if data_home is None:
         data_home = expanduser("~/timefiller_data")
 
@@ -38,16 +37,9 @@ def fetch_pems_bay(data_home=None) -> pd.DataFrame:
 
     # Download the dataset if it doesn't already exist locally
     if not exists(file_path):
-        url = 'https://zenodo.org/records/4263971/files/pems-bay.h5'
-        response = requests.get(url, timeout=60)
-        
-        # Save the content to a temporary file
-        with tempfile.NamedTemporaryFile(suffix='.h5', delete=False) as tmp_file:
-            tmp_file.write(response.content)
-            tmp_file_path = tmp_file.name        
-            # Read the HDF file using pandas
-        df = pd.read_hdf(tmp_file_path).rename_axis(index='time', columns='sensor_id')
-        df.to_csv(file_path)            
+        url = 'https://zenodo.org/records/5724362/files/PEMS-BAY.csv'
+        df = pd.read_csv(url, index_col=0).rename_axis(index='time', columns='sensor_id')
+        df.to_csv(file_path)
 
     # Load the dataset from the local file
     return pd.read_csv(file_path, index_col='time', parse_dates=['time']).asfreq('5min')
