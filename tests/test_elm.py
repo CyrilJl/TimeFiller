2ba7f3c44e12590e519c4d423622a4b6f5f756c1
Cyril <joly.cyril7@gmail.com>
2024-12-06 19:04:11 +0100

    remove one useless test

--- tests/test_elm.py
+++ tests/test_elm.py
@@ -4,7 +4,6 @@ from timefiller import ExtremeLearningMachineRegressor
 
 
 def test_ExtremeLearningMachine():
-    check_estimator(ExtremeLearningMachineRegressor())
     check_estimator(ExtremeLearningMachineRegressor(ratio_features_projection=1.5))
     check_estimator(ExtremeLearningMachineRegressor(ratio_features_projection=2))
     check_estimator(ExtremeLearningMachineRegressor(n_features_projection=10))
