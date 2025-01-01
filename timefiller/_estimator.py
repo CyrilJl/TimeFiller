359be0c83d15e176ab85a4259819e402cbe69b92
CyrilJl <joly.cyril7@gmail.com>
2025-01-01 15:13:38 +0100

    feat: add fit_intercept parameter to FastRidge model for improved flexibility

--- timefiller/_estimator.py
+++ timefiller/_estimator.py
@@ -22,17 +22,18 @@ class FastRidge:
             Predicts target values for the given input data using the fitted model.
     """
 
-    def __init__(self, regularization=1e-2):
+    def __init__(self, fit_intercept=True, regularization=1e-2):
         """
         Initializes the FastRidge model.
 
         Args:
             regularization (float): The regularization strength parameter. Default is 1e-2.
         """
+        self.fit_intercept = fit_intercept
         self.regularization = regularization
 
     def get_params(self, deep=True):
-        return {"regularization": self.regularization}
+        return {"regularization": self.regularization, "fit_intercept": self.fit_intercept}
 
     def fit(self, X, y, sample_weight=None):
         """
@@ -47,19 +48,27 @@ class FastRidge:
         """
         n_samples, n_features = X.shape
 
-        a = np.empty(shape=(n_features + 1, n_features + 1), dtype=X.dtype)
-        a[np.ix_(range(n_features), range(n_features))] = X.T @ X
-        np.fill_diagonal(a, (1 + self.regularization) * a.diagonal())
-        a[-1, :-1] = a[:-1, -1] = X.sum(axis=0)
-        a[-1, -1] = n_samples
+        if self.fit_intercept:
+            a = np.empty(shape=(n_features + 1, n_features + 1), dtype=X.dtype)
+            a[np.ix_(range(n_features), range(n_features))] = X.T @ X
+            np.fill_diagonal(a, (1 + self.regularization) * a.diagonal())
+            a[-1, :-1] = a[:-1, -1] = X.sum(axis=0)
+            a[-1, -1] = n_samples
 
-        b = np.concatenate([X.T @ y, [y.sum()]])
+            b = np.concatenate([X.T @ y, [y.sum()]])
 
-        cho_factorized = cho_factor(a)
-        w = cho_solve(cho_factorized, b)
+            cho_factorized = cho_factor(a)
+            w = cho_solve(cho_factorized, b)
 
-        self.coef_ = w[:-1]
-        self.intercept_ = w[-1]
+            self.coef_ = w[:-1]
+            self.intercept_ = w[-1]
+        else:
+            a = X.T @ X
+            np.fill_diagonal(a, (1 + self.regularization) * a.diagonal())
+            b = X.T @ y
+
+            self.coef_ = np.linalg.solve(a, b)
+            self.intercept_ = 0
 
         return self
 
