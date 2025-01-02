578000b3717245ec29077e8a78bee75159b438a2
Cyril <joly.cyril7@gmail.com>
2025-01-02 14:51:59 +0100

    version v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='timefiller',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=['mapie',
                       'numpy',
