93aad38a602b690ba2ba8bc65083589889d0436e
CyrilJl <joly.cyril7@gmail.com>
2025-01-05 13:36:03 +0100

    ruff format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'timefiller'
-author = 'Cyril Joly'
+project = "timefiller"
+author = "Cyril Joly"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon', 'sphinx_copybutton']
+extensions = ["sphinx.ext.napoleon", "sphinx_copybutton"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 html_sidebars = {"**": []}
 html_logo = "_static/logo_timefiller.svg"
 html_show_sphinx = False
